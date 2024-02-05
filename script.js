ae8a52d0c2c0011e9569f2ca9fcfb0ec1fbfa13f
Gabrielle Ann Franco <156222595+gabfranc@users.noreply.github.com>
2024-02-05 09:08:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showSidebar(){
+  const sidebar = document.querySelector('#sidebar')
+  sidebar.style.display= 'flex'
+}
+
+function hideSidebar(){
+  const sidebar = document.querySelector('#sidebar')
+  sidebar.style.display= 'none'
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide_img");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
